4ad1a112f0743a08dffc98c8884c6b211f354886
psyfluid <psyfluid@gmail.com>
2023-12-18 02:35:51 +0300

    Add homework 3

---
+++ gb-js-1_hw03/1.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*
+Создайте функцию которая возводит переданное число в куб, необходимо вывести в
+консоль результат 2^3 степени + 3 ^ 3 степени
+*/
+
+/**
+ * Calculate third power of input number
+ * @param {number} num original number
+ * @returns {number} third power of number
+ */
+const getPow3 = function calculateThirdPowerOfNumber(num) {
+    return num ** 3;
+}
+
+console.log(getPow3(2) + getPow3(3));
