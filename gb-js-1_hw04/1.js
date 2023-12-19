97a57d735dce8e38c2fb68e8fb3059a7bac151fa
psyfluid <psyfluid@gmail.com>
2023-12-19 22:12:23 +0300

    Refactoring

--- gb-js-1_hw04/1.js
+++ gb-js-1_hw04/1.js
@@ -10,9 +10,12 @@
 10 – четное число
 */
 
-console.log('0 - это ноль');
-
-for (let i = 1; i < 11; i++) {
-  const isEven = i % 2 === 0;
-  console.log(`${i} - ${isEven ? 'четное' : 'нечетное'} число`);
+for (let i = 0; i < 11; i++) {
+  if (i === 0) {
+    console.log(`${i} - это ноль`);
+  } else if (i % 2 === 0) {
+    console.log(`${i} - четное число`);
+  } else {
+    console.log(`${i} - нечетное число`);
+  }
 }
